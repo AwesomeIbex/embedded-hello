fcc61d5c250ed60b66f13840f363749e5fab1869
don <donovandall@icloud.com>
2021-02-09 23:22:08 +0000

    sldfksjdf

--- src/main.rs
+++ src/main.rs
@@ -12,16 +12,18 @@ use stm32f4xx_hal::gpio::Speed; // When a panic occurs, stop the microcontroller
 
 #[entry]
 fn main() -> ! {
+    let hertz = 48.mhz();
+
     let cp = cortex_m::Peripherals::take().unwrap();
     let dp = Peripherals::take().unwrap();
-
     let mut gpioc = dp.GPIOC.split();
+    let rcc = dp.RCC.constrain();
+    let clocks = rcc.cfgr.sysclk(hertz).freeze();
+
     let mut led = gpioc.pc13.into_push_pull_output();
 
-    let rcc = dp.RCC.constrain();
-    let clocks = rcc.cfgr.sysclk(48.mhz()).freeze();
-
     let mut delay = Delay::new(cp.SYST, clocks);
+
     loop {
         led.set_high().unwrap();
         delay.delay_ms(500_u32);
