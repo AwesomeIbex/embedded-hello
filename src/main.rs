9e73b03a2469b694283e935cfb167f028af26716
don <donovandall@icloud.com>
2021-02-15 23:53:18 +0000

    working again

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
 #![no_std]
 #![no_main]
 
-use core::borrow::Borrow;
 use core::cell::RefCell;
-use core::ops::{Deref, DerefMut};
+use core::ops::{DerefMut};
 
 use cortex_m::interrupt::{free, Mutex};
 use cortex_m_rt::entry;
@@ -22,57 +21,63 @@ use stm32f4xx_hal::stm32::Peripherals;
 // When a panic occurs, stop the microcontroller
 
 static GADC: Mutex<RefCell<Option<Adc<stm32::ADC1>>>> = Mutex::new(RefCell::new(None));
-static JOYSTICK_X: Mutex<RefCell<Option<PA0<Analog>>>> = Mutex::new(RefCell::new(None));
-static JOYSTICK_Y: Mutex<RefCell<Option<PA1<Analog>>>> = Mutex::new(RefCell::new(None));
 const MIDDLE_RANGE: u32 = 512;
+const MAX_RANGE: u32 = 4000;
+const LOWEST_RANGE: u32 = 100;
 
 #[entry]
 fn main() -> ! {
-    // let hertz = 48.mhz();
-    //
-    // let cp = cortex_m::Peripherals::take().unwrap();
-    // let dp = Peripherals::take().unwrap();
-    // let gpioc = dp.GPIOC.split();
-    // let rcc = dp.RCC.constrain();
-    // let clocks = rcc.cfgr.sysclk(hertz).freeze();
-    //
-    // let mut led = gpioc.pc13.into_push_pull_output();
-    //
-    // let mut delay = Delay::new(cp.SYST, clocks);
-    //
-    // let adcconfig = AdcConfig::default();
-    // let adc = Adc::adc1(dp.ADC1, true, adcconfig);
-    //
-    // let gpioa = dp.GPIOA.split();
-    // let mut joystick_x = gpioa.pa0.into_analog();
-    // let mut joystick_y = gpioa.pa1.into_analog();
-    //
-    // free(|cs| {
-    //     *GADC.borrow(cs).borrow_mut() = Some(adc);
-    // });
-    //
-    // let mut delay_time = 500_u32;
-    // loop {
-    //     free(|cs| {
-    //         if let Some(ref mut adc) = GADC.borrow(cs).borrow_mut().deref_mut() {
-    //             let x: u32 = adc.read(&mut joystick_x).unwrap() as u32;
-    //             let y: u32 = adc.read(&mut joystick_y).unwrap() as u32;
-    //             if y > MIDDLE_RANGE {
-    //                 delay_time = (delay_time + 50) as u32
-    //             } else if y < MIDDLE_RANGE {
-    //                 delay_time = (delay_time - 50) as u32
-    //             }
-    //             if x > MIDDLE_RANGE {
-    //                 delay_time = 500_u32;
-    //             } else if x < MIDDLE_RANGE {
-    //                 delay_time = 1000_u32;
-    //             }
-    //         }
-    //     });
-    //
-    //     led.set_high().unwrap();
-    //     delay.delay_ms(delay_time);
-    //     led.set_low().unwrap();
-    //     delay.delay_ms(delay_time);
-    loop {};
+    let hertz = 48.mhz();
+
+    let cp = cortex_m::Peripherals::take().unwrap();
+    let dp = Peripherals::take().unwrap();
+    let gpioc = dp.GPIOC.split();
+    let rcc = dp.RCC.constrain();
+    let clocks = rcc.cfgr.sysclk(hertz).freeze();
+
+    let mut led = gpioc.pc13.into_push_pull_output();
+
+    let mut delay = Delay::new(cp.SYST, clocks);
+
+    let adcconfig = AdcConfig::default();
+    let adc = Adc::adc1(dp.ADC1, true, adcconfig);
+
+    let gpioa = dp.GPIOA.split();
+    let mut joystick_x = gpioa.pa0.into_analog();
+    let mut joystick_y = gpioa.pa1.into_analog();
+
+    free(|cs| {
+        *GADC.borrow(cs).borrow_mut() = Some(adc);
+    });
+
+    let mut delay_time = 500_u32;
+    loop {
+        free(|cs| {
+            if let Some(ref mut adc) = GADC.borrow(cs).borrow_mut().deref_mut() {
+                let x: u32 = adc.read(&mut joystick_x).unwrap() as u32;
+                let y: u32 = adc.read(&mut joystick_y).unwrap() as u32;
+
+                if y > 4000 {
+                    led.set_low();
+                } else {
+                    led.set_high();
+                }
+                if y > MAX_RANGE {
+                    delay_time = (delay_time + 50) as u32
+                } else if y < LOWEST_RANGE {
+                    delay_time = (delay_time - 50) as u32
+                }
+                if x > MAX_RANGE {
+                    delay_time = 5000_u32;
+                } else if x < LOWEST_RANGE {
+                    delay_time = 100_u32;
+                }
+            }
+        });
+
+        led.set_high().unwrap();
+        delay.delay_ms(delay_time);
+        led.set_low().unwrap();
+        delay.delay_ms(delay_time);
+    }
 }
